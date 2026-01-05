0792a50a951b97fba9578d2dc6478be9a9122ff7
iamxuanng <nb24082002@gmail.com>
2026-01-06 00:01:18 +0700

    feat(rf): add training and test script for random forest model

---
+++ src/test/rf/rf_test.py
@@ -0,0 +1,22 @@
+import joblib
+from src.constant import PROCESSED_OUTPUTS_TEST, PROCESSED_FEATURES
+from src.utils.io import read_multi_csv
+from src.utils.visualization import show_confusion_matrix
+
+# === Đọc dữ liệu test đã xử lý ===
+df_test = read_multi_csv(PROCESSED_OUTPUTS_TEST)
+df_test = df_test[PROCESSED_FEATURES]
+
+X_test = df_test.drop('label', axis=1).values
+y_test = df_test['label'].values
+
+# === Load mô hình Random Forest đã train ===
+model_path = 'D:/DOCUMENTS/SEMESTER/20251/Đồ án tốt nghiệp/CODE/models/rf/cicddos_rf_model.joblib'
+rf_model = joblib.load(model_path)
+
+# === Dự đoán ===
+y_pred = rf_model.predict(X_test)
+
+# === Đánh giá trực quan & in báo cáo ===
+show_confusion_matrix(y_test, y_pred, class_names=["Benign", "Attack"])
+
